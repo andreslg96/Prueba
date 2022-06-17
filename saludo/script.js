f7eb6d943cafd0c2df34e7fee274b66b4b19d936
Andrés López <andres.lg96@outlook.com>
2022-06-17 15:21:30 -0500

    doc: documentación de html y css

--- saludo/script.js
+++ saludo/script.js
@@ -2,10 +2,14 @@ console.log("Se nota que todavía me extrañas.")
 
 setInterval(cambiarSaludo,2000);
 
+/**
+ * Cambia la frase en el título h2
+ */
 function cambiarSaludo(){
     let frase = document.getElementsByTagName("h2")[1].innerHTML;
     
     //console.log("Me parece una falta de respeto");
+    //Estoy asignando a una variable el valor de h2
     if(document.getElementsByTagName("h2")[1].innerHTML =="Ganando como siempre"){
         document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!"
     }
