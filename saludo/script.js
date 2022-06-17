6cf3d75b74d88eef8c5b3fb59f35c01d2092c392
Andrés López <andres.lg96@outlook.com>
2022-06-17 15:11:15 -0500

    Cambio a frase

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,17 @@
 console.log("Se nota que todavía me extrañas.")
+
+setInterval(cambiarSaludo,2000);
+
+function cambiarSaludo(){
+    let frase = document.getElementsByTagName("h2")[1].innerHTML;
+    
+    //console.log("Me parece una falta de respeto");
+    if(document.getElementsByTagName("h2")[1].innerHTML =="Ganando como siempre"){
+        document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!"
+    }
+    else{
+        document.getElementsByTagName("h2")[1].innerHTML="Ganando como siempre"
+    }
+    console.log("frase: "+frase)
+ 
+}
